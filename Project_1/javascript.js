d4993496dcd633bf1c4b7408ca5c24412928ae54
JennyFarag <jennyfarag17@gmail.com>
2024-06-07 21:58:26 -0400

    trying to make learn more button work
    learn more

--- Project_1/javascript.js
+++ Project_1/javascript.js
@@ -30,3 +30,10 @@ document.getElementById('appointmentForm').addEventListener('submit', function (
     modal.show();
 });
 
+ // Enable Bootstrap popover
+ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+     return new bootstrap.Popover(popoverTriggerEl)
+ })
+
+
